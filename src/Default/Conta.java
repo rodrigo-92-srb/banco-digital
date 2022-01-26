7566487a756a80bd5f0f25cf442d8de8439101b3
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2022-01-25 21:58:23 -0300

    Polimorfismo

--- src/Default/Conta.java
+++ src/Default/Conta.java
@@ -1,9 +1,18 @@
 package Default;
 
 public abstract class Conta implements IConta{
-    private int agencia;
-    private int numero;
-    private double saldo;
+
+    private static  final int AGENCIA_PADRAO = 0001;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta(){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     public int getAgencia() {
         return agencia;
@@ -17,17 +26,14 @@ public abstract class Conta implements IConta{
         return saldo;
     }
 
-    @Override
     public void sacar(double valor) {
 
     }
 
-    @Override
     public void depositar(double valor) {
 
     }
 
-    @Override
     public void tranferir(double valor, Conta contaDestino) {
 
     }
--- src/Default/ContaCorrente.java
+++ src/Default/ContaCorrente.java
@@ -2,4 +2,5 @@ package Default;
 
 public class ContaCorrente extends Conta{
 
+
 }
